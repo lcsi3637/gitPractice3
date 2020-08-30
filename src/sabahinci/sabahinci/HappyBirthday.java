d2d49e7895b27328be5dac9a96fd762bc2d90bc1
lcsi3637@gmail.com <Q7hao2604@>
2020-08-29 23:37:34 -0400

    1

--- src/sabahinci/sabahinci/HappyBirthday.java
+++
@@ -1,9 +0,0 @@
-package sabahinci;
-
-public class HappyBirthday {
-    public static void main(String[] args) {
-
-
-        System.out.println("saban inci");
-    }
-}
--- src/sabahinci/sabahinci/baris.java
+++
@@ -1,8 +0,0 @@
-package sabahinci;
-
-public class baris {
-    public static void main(String[] args) {
-        System.out.println("abi boyle yapma ");
-        System.out.println("dfvf");
-    }
-}
